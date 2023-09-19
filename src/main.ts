fecd9b55ed6e01aee0eae258b7ae787ffd7a6bad
Krystian Koc <krystian582@gmail.com>
2023-09-19 14:41:47 +0200

    Add basic setup of ngrx store

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,16 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { importProvidersFrom } from '@angular/core';
 import { AppRoutingModule } from './app/app-routing.module';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 // platformBrowserDynamic().bootstrapModule(AppModule)
 //   .catch(err => console.error(err));
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(AppRoutingModule)
+    importProvidersFrom(AppRoutingModule),
+    provideStore(),
+    provideEffects(),
   ],
 });
