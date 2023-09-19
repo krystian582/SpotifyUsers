26c8a32cbac20d202689394ccfb06f34ed5fab3a
Krystian Koc <krystian582@gmail.com>
2023-09-19 14:25:17 +0200

    Change project to standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  imports: [
+  ]
 })
 export class AppComponent {
   title = 'SpotifyUsers';
